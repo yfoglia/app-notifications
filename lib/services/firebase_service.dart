dc1ead6395faa0187a9f058901cb207ff3efe302
Yamil Foglia <yamil.foglia@gmail.com>
2023-07-11 00:26:59 -0300

    add funtion for real time firebase

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -6,37 +6,52 @@ FirebaseFirestore database = FirebaseFirestore.instance;
 CollectionReference collectionReferenceData = database.collection('product');
 
 Future<List> getProduct() async {
-	List products = [];
+  List products = [];
 
-	QuerySnapshot queryData = await collectionReferenceData.get();
+  QuerySnapshot queryData = await collectionReferenceData.get();
 
-	for (var doc in queryData.docs) {
+  for (var doc in queryData.docs) {
+    final Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    final element = {
+      "name": data['name'],
+      "code": data['code'],
+      "expirationDate": data['expirationDate'],
+      "firebaseId": doc.id,
+    };
 
-		final Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-		final element = {
-			"name": data['name'],
-			"code": data['code'],
-			"expirationDate": data['expirationDate'],
-			"firebaseId": doc.id,
-		};
+    products.add(element);
+  }
 
-		products.add(element);
+  return products;
+}
 
-	}
-
-	return products;
+Stream<List<Map<String, dynamic>>> streamProduct() {
+  return collectionReferenceData.snapshots().map((snapshot) {
+    List<Map<String, dynamic>> products = [];
+    snapshot.docs.forEach((doc) {
+      final Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+      final element = {
+        "name": data['name'],
+        "code": data['code'],
+        "expirationDate": data['expirationDate'],
+        "firebaseId": doc.id,
+      };
+      products.add(element);
+    });
+    return products;
+  });
 }
 
 Future<void> addProduct(Product newElement) async {
-	Map<String, dynamic> elementData = newElement.toJson();
-	await collectionReferenceData.add(elementData);
+  Map<String, dynamic> elementData = newElement.toJson();
+  await collectionReferenceData.add(elementData);
 }
 
 Future<void> updateProduct(String idFirebase, Product newElement) async {
-	Map<String, dynamic> elementData = newElement.toJson();
-	await collectionReferenceData.doc(idFirebase).set(elementData);
+  Map<String, dynamic> elementData = newElement.toJson();
+  await collectionReferenceData.doc(idFirebase).set(elementData);
 }
 
 Future<void> deleteProduct(String idFirebase) async {
-	await collectionReferenceData.doc(idFirebase).delete();
+  await collectionReferenceData.doc(idFirebase).delete();
 }
